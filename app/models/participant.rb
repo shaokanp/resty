8c21f5d12ae9fb3952b2b0fff5b824df991acb7d
shaokanp <whobirdsyou@gmail.com>
2013-07-15 00:19:04 +0800

    Remove User

--- app/models/participant.rb
+++
@@ -1,4 +0,0 @@
-class Participant < ActiveRecord::Base
-	belongs_to :users
-	belongs_to :projects
-end
--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-	has_many :participants
-	has_many :projects, through: :participants
-end
